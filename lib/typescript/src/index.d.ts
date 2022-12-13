8223ea214938fb1f16ca3bf66d0b6a8ffe9a778c
Vytautas Saulis <vaulis11@gmail.com>
2022-12-13 22:17:20 +0200

    Downgrade

--- lib/typescript/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { defaultCustomization } from './customization';
-declare const FaceTecView: ({ show, onStateUpdate, vocalGuidanceMode, config, customization, }: Types.FaceTecViewProps) => JSX.Element | null;
-type FaceTecViewProps = Types.FaceTecViewProps;
-type FaceTecConfig = Types.FaceTecConfig;
-type FaceTecStatus = Types.FaceTecStatus;
-type FaceTecState = Types.FaceTecState;
-type FaceTecLoad = Types.FaceTecLoad;
-export { defaultCustomization, FaceTecView, FaceTecViewProps, FaceTecConfig, FaceTecStatus, FaceTecState, FaceTecLoad, };
